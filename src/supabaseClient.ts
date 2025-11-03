99ac53a1f11d6fee5ab2f14ac0bb709c00be332c
Johan-C8 <edisonjohan1234@gmail.com>
2025-11-02 21:00:22 -0500

    Fix TypeScript errors and add vite-env.d.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,10 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Faltan las variables de entorno de Supabase')
-}
+const supabaseUrl = 'https://tbmdwlthhuddwomqydno.supabase.co' 
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRibWR3bHRoaHVkZHdvbXF5ZG5vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5MzcxMzgsImV4cCI6MjA3NzUxMzEzOH0.cyS8fSX_im61m-Rwhptm0EpeaXqfYSQVBdZhvni9JIg'  
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
